6e6693fd6ccceb1f1b0db5e4c4e1f04946f1883c
mael <mael.pidjou@gmail.com>
2018-12-14 19:02:20 +0900

    fourth commit

--- src/components/Courses/ViewCourses.vue
+++ src/components/Courses/ViewCourses.vue
@@ -1,5 +1,115 @@
 <template>
-	
+	<v-container>
+	  <v-layout >
+	    <v-flex xs4 sm3 class="pr-4">
+	      <v-card>
+	        <v-img
+	      
+	          src="https://bit.ly/2A05Ie7"
+	          aspect-ratio="1"
+
+	        ></v-img>
+
+	        <v-card-title primary-title>
+	          <div>
+	            <h3 class="headline mb-0">TOMOKA</h3>
+	            <div>MATHS
+	            <br>JAPAN
+				<br>JAPANESE	
+	        	</div>
+	          </div>
+	        </v-card-title>
+
+	        <v-card-actions>
+	          <v-btn flat color="success">View details</v-btn>
+	        </v-card-actions>
+	      </v-card>
+	    </v-flex>
+	    <v-flex xs4 sm3 class="pr-4">
+	      <v-card>
+	        <v-img
+	      
+	          src="https://bit.ly/2A05Ie7"
+	          aspect-ratio="1"
+
+	        ></v-img>
+
+	        <v-card-title primary-title>
+	          <div>
+	            <h3 class="headline mb-0">MAEL</h3>
+	            <div>PHYSICS
+	            <br>CAMEROON
+				<br>FRENCH	
+	        	</div>
+	          </div>
+	        </v-card-title>
+
+	        <v-card-actions>
+	          <v-btn flat color="success">View details</v-btn>
+	        </v-card-actions>
+	      </v-card>
+	    </v-flex>
+	    <v-flex xs4 sm3 class="pr-4">
+	      <v-card>
+	        <v-img
+	      
+	          src="https://bit.ly/2A05Ie7"
+	          aspect-ratio="1"
+	  
+	        ></v-img>
+
+	        <v-card-title primary-title>
+	          <div>
+	            <h3 class="headline mb-0">ELENA</h3>
+	            <div>COMPUTER SCIENCE
+	            <br>GREECE
+				<br>GREEK	
+	        	</div>
+	          </div>
+	        </v-card-title>
+
+	        <v-card-actions>
+	          <v-btn flat color="success">View details</v-btn>
+	        </v-card-actions>
+	      </v-card>
+	    </v-flex>
+	    <v-flex xs4 sm3 class="pr-4">
+	      <v-card>
+	        <v-img
+	      
+	          src="https://bit.ly/2A05Ie7"
+	          aspect-ratio="1"
+	  
+	        ></v-img>
+
+	        <v-card-title primary-title>
+	          <div>
+	            <h3 class="headline mb-0">TOMOKA</h3>
+	            <div>MATHS
+	            <br>JAPAN
+				<br>JAPANESE	
+	        	</div>
+	          </div>
+	        </v-card-title>
+
+	        <v-card-actions>
+	          <v-btn flat color="success">View details</v-btn>
+	        </v-card-actions>
+	      </v-card>
+	    </v-flex>
+	  </v-layout>
+	</v-container>
+  
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+      }
+    }
+  }
+</script>
 
 
--- src/components/Users/Profile.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div>
-			<P>profile of users</P>
-	</div>
-
-</template>
